ff119e44674761758f4f73835b268fa6f0c34f69
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-07-04 13:17:28 +0200

    Adds the basics / primitives and some documentation.

---
+++ primitives.py
@@ -0,0 +1,83 @@
+import utils
+
+def elementwise(op, parameters, number_of_dimensions=None):
+    """ Repesents an operation which can be applied concurrently, because the invokations are independent and do not influence each other.
+    Multiple parameter elements (represented as a tuple) can be combined into a single element in each invokation.
+    ----------
+    op : Operation to apply to each element tuple
+    parameters : List of element lists, which will be transposed to element tuples
+    number_of_dimensions : Nesting level of the element lists
+    """
+    return utils.flatten_and_structure_dimensions(lambda arguments: map(op, arguments), parameters, number_of_dimensions)
+
+def gather(permutation, elements):
+    """ Permutation.
+    Elements are discarded when no index in the permutation refers to them.
+    Elements are replicated when multiple indices in the permutation refer to them.
+    If len(permutation) < len(elements), then some elements must be discarded.
+    If len(permutation) > len(elements), then some elements must be replicated.
+    Elements can still be discarded and replicated even if len(permutation) == len(elements), depending on the indices.
+    The output will have len(permutation) elements.
+    ----------
+    permutation : List of indices
+    elements : List of elements
+    """
+    indices = range(0, len(permutation))
+    return elementwise(lambda (index): elements[permutation[index]], (indices), 1)
+
+def scatter(permutation, elements, condition_flags=None, result_size=None):
+    """ Permutation Inverse.
+    The permutation and elements must form matching pairs: len(permutation) == len(elements).
+    If condition_flags are given, they must also match the permutation and elements: len(permutation) == len(result_size) and len(elements) == len(result_size).
+    Elements are discarded when condition_flags are given and the specific flag of the element is set to false.
+    Elements can not be replicated as each element can only be moved to one index (not a set of indices).
+    If multiple indices collide, the resulting element at that index is undefined. Thus, this should be avoided.
+    The output will have result_size elements or len(permutation) if result_size is not given.
+    The resulting elements not referred to by any index are undefined.
+    ----------
+    permutation : List of indices
+    elements : List of elements
+    condition_flags : List of boolean flags
+    result_size : Expected number of flags set to "true" in condition_flags, for memory allocation.
+    """
+    if result_size == None:
+        result_size = len(permutation)
+    result = [0]*result_size
+    def store(index):
+        result[permutation[index]] = elements[index]
+    def conditional_store(index):
+        if condition_flags[index]:
+            store(index)
+    indices = range(0, len(permutation))
+    elementwise(store if condition_flags == None else conditional_store, (indices), 1)
+    return result
+
+def reduction(op, elements, accumulator):
+    """ Combines a list of elements into one using an accumulator. Returns the final state of the accumulator.
+    ----------
+    op : Operation to apply to each element and the accumulator. Must be associative and distributive.
+    elements : List of elements
+    accumulator : Initial state of the accumulator
+    """
+    for element in elements:
+        accumulator = op((accumulator, element))
+    return accumulator # reduce(op, elements, accumulator)
+
+def integral(shift, op, elements, accumulator):
+    """ Scans a list of elements using an accumulator. Returns the integral of the elements and the final state of the accumulator.
+    ----------
+    shift : Shift of -1 is an exclusive scan and a shift of 1 is an inclusive scan.
+    op : Operation to apply to each element and the accumulator. Must be associative and distributive.
+    elements : List of elements
+    accumulator : Initial state of the accumulator
+    """
+    integral = [0]*len(elements)
+    if shift == -1:
+        for i in range(0, len(elements)):
+            integral[i] = accumulator
+            accumulator = op((accumulator, elements[i]))
+    elif shift == 1:
+        for i in range(0, len(elements)):
+            accumulator = op((accumulator, elements[i]))
+            integral[i] = accumulator
+    return (integral, accumulator)
--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-# Cast boolean to unsigned int
 def condition_to_flag(condition):
+    """ Cast boolean to unsigned int.
+    """
     return 1 if condition else 0
 
-# Gets the number of elements in each dimension, and implicitly also how many nested dimensions there are
 def get_dimensions(elements):
+    """ Returns the number of elements in each dimension as a list, and implicitly also how many nested dimensions there are.
+    Dimensions and ranks are in "little endian".
+    The most inner / minor dimension (least significant) comes first and the most outer / major dimension (most significant) last.
+    """
     dimensions = []
     inner_elements = elements
     while isinstance(inner_elements, list):
@@ -15,8 +19,12 @@ def get_dimensions(elements):
 def flatten_one_dimension(elements):
     return [lowerer_rank for middle_rank in elements for lowerer_rank in middle_rank]
 
-# Unrolls nested lists into one flat list
 def flatten_dimensions(elements, number_of_dimensions=None):
+    """ Unrolls nested lists into one flat list.
+    ----------
+    elements : The  nested lists
+    number_of_dimensions : How many of the outer dimensions to peel of, leaving the inner dimensions nested
+    """
     dimensions = get_dimensions(elements)
     if number_of_dimensions == None:
         number_of_dimensions = len(dimensions)
@@ -27,13 +35,19 @@ def flatten_dimensions(elements, number_of_dimensions=None):
 def structure_one_dimension(dimension, elements):
     return [elements[offset:offset+dimension] for offset in range(0, len(elements), dimension)]
 
-# Rolls a flat list back into nested lists
 def structure_dimensions(dimensions, elements):
+    """ Rolls a flat list back into nested lists.
+    ----------
+    dimensions : The number of elements in each dimension, as returned by get_dimensions(elements).
+    elements : The flat list
+    """
     for dimension_index in range(0, len(dimensions)-1):
         elements = structure_one_dimension(dimensions[dimension_index], elements)
     return elements
 
 def flatten_and_structure_dimensions(op, parameters, number_of_dimensions=None):
+    """ Unrolls nested lists into one flat lists, applies the operation and rolls the resulting flat list back into nested lists.
+    """
     multi_args = isinstance(parameters, tuple)
     dimensions_and_arguments = map(lambda parameter: flatten_dimensions(parameter, number_of_dimensions), parameters if multi_args else [parameters])
     arguments = map(lambda (dimensions, argument): argument, dimensions_and_arguments)
