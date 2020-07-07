26709c9ac291c9b031442e33c4eea7aa923848d4
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-07-07 07:39:45 +0200

    Adds linear algebra algorithms

--- algorithms.py
+++ algorithms.py
@@ -87,3 +87,60 @@ def dst_transposition(rank_permutation, elements):
     index_permutation = primitives.elementwise(lambda (dst_ranks): element_ranks_to_index(dst_dimensions, dst_ranks), (dst_ranks_per_element), 1)
     permutated_elements = primitives.gather(index_permutation, flattened)
     return utils.structure_dimensions(src_dimensions, permutated_elements)
+
+def inner_product(combination_op, reduction_op, accumulator, elements_a, elements_b):
+    """ Computes the inner product of two nested lists of elements, by first combining them elementwise and then reducing everything into one element.
+    ----------
+    combination_op : Operation to apply to each element pair to get a combined element.
+    reduction_op : Reduction operation to apply to each combined element and the accumulator. Must be associative and distributive.
+    accumulator : Initial state of the reduction accumulator.
+    elements_a : Nested lists of elements.
+    elements_b : Nested lists of elements.
+    """
+    combined_elements = primitives.elementwise(combination_op, (elements_a, elements_b))
+    (dimensions, flattened) = utils.flatten_dimensions(combined_elements)
+    return primitives.reduction(reduction_op, flattened, accumulator)
+
+def outer_product(combination_op, elements_a, elements_b, number_of_dimensions_a=None, number_of_dimensions_b=None):
+    """ Computes the outer product of two nested lists of elements.
+    ----------
+    combination_op : Operation to apply to each element pair to get a combined element.
+    elements_a : Nested lists of elements.
+    elements_b : Nested lists of elements.
+    number_of_dimensions_a : How many of the outer dimensions of elements_a to peel of, leaving the inner dimensions nested.
+    number_of_dimensions_b : How many of the outer dimensions of elements_b to peel of, leaving the inner dimensions nested.
+    """
+    def for_each_element_a(combination_op, element_a, elements_b):
+        return utils.flatten_and_structure_dimensions(lambda (flattened_b): primitives.elementwise(lambda (element_b): combination_op((element_a, element_b)), (flattened_b), 1), (elements_b), number_of_dimensions_b)
+    return utils.flatten_and_structure_dimensions(lambda (flattened_a): primitives.elementwise(lambda (element_a): for_each_element_a(combination_op, element_a, elements_b), (flattened_a), 1), (elements_a), number_of_dimensions_a)
+
+def dot_product(vector_a, vector_b):
+    """ Computes the dot product of two vectors, resulting in a scalar.
+    ----------
+    vector_a : List of elements.
+    vector_b : List of elements.
+    """
+    return inner_product(lambda (a, b): a*b, lambda (a, b): a+b, 0, vector_a, vector_b)
+
+def matrix_multiplication(matrix_a, matrix_b):
+    """ Computes the product of two matrices, resulting in another matrix.
+    ----------
+    matrix_a : Nested lists of elements.
+    matrix_b : Nested lists of elements.
+    """
+    transposed_matrix_b = src_transposition([1, 0], matrix_b)
+    return outer_product(lambda (vector_a, vector_b): dot_product(vector_a, vector_b), matrix_a, transposed_matrix_b, 1, 1)
+
+def vector_transformation(matrix_a, vector_b):
+    """ Computes the product of a matrix and a vector, resulting in another vector.
+    ----------
+    matrix_a : Nested lists of elements.
+    vector_b : List of elements.
+    """
+    # Optimization: Transposition and inverse transposition cancel each other out
+    # matrix_b = src_transposition([1, 0], [vector_b])
+    # transformed_vector = matrix_multiplication(matrix_a, matrix_b)
+    transformed_vector = outer_product(lambda (vector_a, vector_b): dot_product(vector_a, vector_b), matrix_a, [vector_b], 1, 1)
+    # Optimization: Transpose and unpack the vector
+    # return src_transposition([1, 0], transformed_vector)[0]
+    return utils.flatten_dimensions(transformed_vector)[1]
